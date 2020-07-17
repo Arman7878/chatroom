b7530e3ea5808d06f6ea61f161cec178eaf970e6
armanexplorer <armanexplorer@gmail.com>
2020-07-18 04:15:06 +0430

    some bugs fixed

--- index.js
+++ index.js
@@ -13,16 +13,18 @@ io.on('connection', (socket) => {
         console.log('user disconnected');
       });
     socket.on('chat message', (msg) => {
-        chat = `${socket.color}:${socket.username}:${msg}`
-        io.emit('chat message', chat);
-        chats.push(chat)
+        data = {color: socket.color, username: socket.username, txt: msg}
+        io.to('chatroom').emit('chat message', data);
+        chats.push(data)
+        // console.log(chats);
     });
     socket.on('register', (name) => {
         color = "#"+((1<<24)*Math.random()|0).toString(16)
         socket.username = name;
         socket.color = color
         console.log(`the user with name ${socket.username} is registered`)
-        io.emit('get messages', chats);
+        io.to(socket.id).emit('get messages', chats);
+        socket.join('chatroom');
     });
 });
 
